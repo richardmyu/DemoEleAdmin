9f2c7d718d359e6662789cfa7ab6241bda4a8c15
yum <richardminyu@foxmail.com>
2020-12-20 06:48:40 +0800

    add reset

---
+++ src/settings.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'Vue Admin Template',
+  /**
+   * @type {boolean}
+   * @description whether fix the header
+   */
+  fixedHeader: false,
+  /**
+   * @type {boolean}
+   * @description whether show the logo in sidebar
+   */
+  sidebarLogo: false
+};
