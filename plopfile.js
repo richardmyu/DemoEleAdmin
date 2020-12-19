0ac97d2a2e127048dee0d08377ec1beb6596fde4
yum <richardminyu@foxmail.com>
2020-12-20 07:40:36 +0800

    del all

--- plopfile.js
+++
@@ -1,9 +0,0 @@
-const viewGenerator = require('./plop-templates/view/prompt');
-const componentGenerator = require('./plop-templates/component/prompt');
-const storeGenerator = require('./plop-templates/store/prompt');
-
-module.exports = function (plop) {
-  plop.storeGenerator('view', viewGenerator);
-  plop.storeGenerator('component', componentGenerator);
-  plop.storeGenerator('store', storeGenerator);
-};
