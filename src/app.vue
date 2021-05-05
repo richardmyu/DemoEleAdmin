4a8d500fc819c80c855739714c14b8635feab7a3
yum <richardminyu@foxmail.com>
2021-05-05 22:22:36 +0800

    ,

---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <route-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name:'APP',
+  data(){
+    return {}
+  }
+}
+</script>
+
+<style scoped lang=''></style>
